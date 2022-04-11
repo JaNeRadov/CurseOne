0fb3a5b00a5772a49ee4d52064f595307dcd502e
Yuriy Zarubin <temisfucincrazy@icloud.com>
2022-04-11 15:25:13 +0300

    пятая домашка

--- 5I-ZarubinYuriy/5I-ZarubinYuriy/main.swift
+++ 5I-ZarubinYuriy/5I-ZarubinYuriy/main.swift
@@ -4,188 +4,189 @@
 //
 //  Created by Jane Z. on 08.04.2022.
 //
-
+//
+import Foundation
 import Foundation
 
-// MARK: - ENUMы
- 
-///Engine Status Enum
+ // MARK: - ENUMы
+  
+ ///Engine Status Enum
 
-enum EnginStatus {
-    
-    case start
-    case stop
-    
-}
+ enum EnginStatus {
 
-/// Door status
+     case start
+     case stop
 
-enum DoorStatus {
-    
-    case open
-    case close
-    
-}
+ }
 
-///Windows Status
+ /// Door status
 
-enum WindowsStatus {
-    
-    case open
-    case close
+ enum DoorStatus {
 
-}
+     case open
+     case close
 
-///BackDoor Status
+ }
 
-enum BacDoorStatus {
-    
-    case full
-    case empty
-    
-}
-///Pogruzka
+ ///Windows Status
 
-enum Pogruzka {
-    
-    case loading
-    case complted
-    
-}
-///
+ enum WindowsStatus {
 
-enum HatchStatus {
-    
-    case open
-    case close
-    
-}
+     case open
+     case close
 
-enum SignalisationStatus {
-    
-    case on
-    case off
-    
-}
+ }
 
-enum LightStatus {
-    
-    case lightOn
-    case lightOff
-}
+ ///BackDoor Status
 
-// MARK: - Протокол
+ enum BacDoorStatus {
 
-protocol CarProtocol {
-    
-    var engine: EnginStatus { get }
-    var windows: WindowsStatus { get }
-    var door: DoorStatus { get }
-    var bacDoor: BacDoorStatus { get }
-    
-}
+     case full
+     case empty
 
+ }
+ ///Pogruzka
 
+ enum Pogruzka {
 
-// MARK: -Class
+     case loading
+     case complted
 
-final class SportCar: CarProtocol {
-    
-    var engine: EnginStatus
-    
-    var windows: WindowsStatus
-    
-    var door: DoorStatus
-    
-    var bacDoor: BacDoorStatus
-    
-    var lights: LightStatus
-    
-    init (engine: EnginStatus,
-          windows: WindowsStatus,
-          door: DoorStatus,
-          bacdoor: BacDoorStatus,
-          lights: LightStatus){
-        
-        self.engine = engine
-        self.bacDoor = bacdoor
-        self.windows = windows
-        self.door = door
-        self.lights = lights
-    }
-    
-}
+ }
+ ///
 
-final class TruckCar:CarProtocol {
-    
-    var engine: EnginStatus
-    
-    var windows: WindowsStatus
-    
-    var door: DoorStatus
-    
-    var bacDoor: BacDoorStatus
-    
-    var signalization: SignalisationStatus
-    
-    init (engine: EnginStatus,
-          windows: WindowsStatus,
-          door: DoorStatus,
-          bacdoor: BacDoorStatus,
-          signalization: SignalisationStatus){
-        
-        self.engine = engine
-        self.bacDoor = bacdoor
-        self.windows = windows
-        self.door = door
-        self.signalization = signalization
-    }
-}
+ enum HatchStatus {
 
-// MARK: -Extension
+     case open
+     case close
 
-extension CarProtocol {
-    
-    func printCarStatus() {
-        
-        print ("Car ready to driving")
-    
-    }
-    
-}
+ }
 
-extension TruckCar {
-    func printSignalStatus(){
-        print ("Signalisation Off")
-    }
-}
+ enum SignalisationStatus {
 
-let porsche  = SportCar(engine: .start,
-                        windows: .close,
-                        door: .close,
-                        bacdoor: .empty,
-                        lights: .lightOn)
+     case on
+     case off
 
-let volvo  = TruckCar(engine: .start,
-                        windows: .close,
-                        door: .close,
-                        bacdoor: .empty,
-                      signalization: .off)
+ }
+
+ enum LightStatus {
+
+     case lightOn
+     case lightOff
+ }
+
+ // MARK: - Протокол
+
+ protocol CarProtocol {
+
+     var engine: EnginStatus { get }
+     var windows: WindowsStatus { get }
+     var door: DoorStatus { get }
+     var bacDoor: BacDoorStatus { get }
+
+ }
 
 
-print (porsche.printCarStatus(), ",",
-       porsche.windows, ",",
-       porsche.lights, ",",
-       porsche.door, ",",
-       porsche.bacDoor, ",",
-       porsche.engine, ",")
 
-print("===============================")
+ // MARK: -Class
 
-print(volvo.signalization, ",",
-      volvo.engine, ",",
-      volvo.bacDoor, ",",
-      volvo.door, ",",
-      volvo.windows, ",",
-      volvo.printSignalStatus(),",",
-      volvo.printCarStatus())
+ final class SportCar: CarProtocol {
+
+     var engine: EnginStatus
+
+     var windows: WindowsStatus
+
+     var door: DoorStatus
+
+     var bacDoor: BacDoorStatus
+
+     var lights: LightStatus
+
+     init (engine: EnginStatus,
+           windows: WindowsStatus,
+           door: DoorStatus,
+           bacdoor: BacDoorStatus,
+           lights: LightStatus){
+
+         self.engine = engine
+         self.bacDoor = bacdoor
+         self.windows = windows
+         self.door = door
+         self.lights = lights
+     }
+
+ }
+
+ final class TruckCar:CarProtocol {
+
+     var engine: EnginStatus
+
+     var windows: WindowsStatus
+
+     var door: DoorStatus
+
+     var bacDoor: BacDoorStatus
+
+     var signalization: SignalisationStatus
+
+     init (engine: EnginStatus,
+           windows: WindowsStatus,
+           door: DoorStatus,
+           bacdoor: BacDoorStatus,
+           signalization: SignalisationStatus){
+
+         self.engine = engine
+         self.bacDoor = bacdoor
+         self.windows = windows
+         self.door = door
+         self.signalization = signalization
+     }
+ }
+
+ // MARK: -Extension
+
+ extension CarProtocol {
+
+     func printCarStatus() {
+
+         print ("Car ready to driving")
+
+     }
+
+ }
+
+ extension TruckCar {
+     func printSignalStatus(){
+         print ("Signalisation Off")
+     }
+ }
+
+ let porsche  = SportCar(engine: .start,
+                         windows: .close,
+                         door: .close,
+                         bacdoor: .empty,
+                         lights: .lightOn)
+
+ let volvo  = TruckCar(engine: .start,
+                         windows: .close,
+                         door: .close,
+                         bacdoor: .empty,
+                       signalization: .off)
+
+
+ print (porsche.printCarStatus(), ",",
+        porsche.windows, ",",
+        porsche.lights, ",",
+        porsche.door, ",",
+        porsche.bacDoor, ",",
+        porsche.engine, ",")
+
+ print("===============================")
+
+ print(volvo.signalization, ",",
+       volvo.engine, ",",
+       volvo.bacDoor, ",",
+       volvo.door, ",",
+       volvo.windows, ",",
+       volvo.printSignalStatus(),",",
+       volvo.printCarStatus())
