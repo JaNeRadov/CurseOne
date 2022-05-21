00320ccbf32fe11ac9b2b368504ab82aee83c88e
Yuriy Zarubin <temisfucincrazy@icloud.com>
2022-05-21 18:03:47 +0300

    no message

---
+++ GBZarubin/GBZarubin/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GBZarubin
+//
+//  Created by Jane Z. on 30.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var MainImage: UIImageView!
+    
+    @IBAction func ButtonSignIn(_ sender: Any) {
+    }
+}
+
---
+++ VkZarubin/VkZarubin/VkZarubin/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  VkZarubin
+//
+//  Created by Jane Z. on 03.05.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
